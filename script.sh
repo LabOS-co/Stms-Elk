a5da60df2eedf99d8a7388837c17323ea4fa9b8a
netlims <moran@stms.co.il>
2019-08-22 11:14:12 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,21 +1,35 @@
-curl -XPUT -D- '/_ilm/policy/logstash_clean_policy?pretty'  -H 'Content-Type: application/json' -d '{
-    "policy": {
-        "phases": {
-            "hot": {
-                "min_age": "0ms",
-                "actions": {
-                    "set_priority": {
-                        "priority": 100
-                    }
-                }
-            },
-            "delete": {
-                "min_age": "14d",
-                "actions": {
-                    "delete": {}
-                }
-            }
+curl -XPUT '/_ilm/policy/logstash_clean_policy1?pretty'  
+-H 'Content-Type: application/json' 
+-d '{
+  "policy": {
+    "phases": {
+      "hot": {
+        "min_age": "0ms",
+        "actions": {
+          "set_priority": {
+            "priority": 100
+          }
         }
+      },
+      "delete": {
+        "min_age": "14d",
+        "actions": {
+          "delete": {}
+        }
+      }
     }
+  }
 }'
 
+
+PUT /_template/logstash_clean_policy?pretty
+{
+  "index_patterns": [
+    "logstash-*"
+  ],
+  "settings": {
+    "number_of_shards": 1,
+    "number_of_replicas": 1,
+    "index.lifecycle.name": "logstash_clean_policy"
+  }
+}
