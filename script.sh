ad7d4b5026c65986cdbfc57831a7ffb681cd263a
netlims <moran@stms.co.il>
2019-08-25 15:51:47 +0300

    11

--- script.sh
+++ script.sh
@@ -5,17 +5,13 @@ curl -X PUT "http://elasticsearch:9201/_template/softov_log" -H 'Content-Type: a
 # we dont want to create the index pattern over and over again, so I am cheching if it exists first
 
 code=$(curl -X GET "kibana:5601/api/saved_objects/_find?type=index-pattern&search_fields=title&search=logstash*" -H 'kbn-xsrf: true')
-# if [[ $code =~ '"total":0' ]] ; then
 
-case '"total":0' in $code )
-	curl -X POST -D- 'http://kibana:5601/api/saved_objects/index-pattern' \
+curl -X POST -D- 'http://kibana:5601/api/saved_objects/index-pattern' \
 		-H 'Content-Type: application/json' \
 		-H 'kbn-version: 7.2.0' \
 		-d '{"attributes":{"title":"logstash*","timeFieldName":"@timestamp"}}'
 		;;
-esac
-
-#fi
+fi
 	
 	
 curl -X PUT 'elasticsearch:9201/_ilm/policy/logstash_clean_policy?pretty' -H 'Content-Type: application/json' -d '
