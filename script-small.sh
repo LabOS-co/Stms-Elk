ae33ce3b2e1baa9106600b9787a5ff6036002430
netlims <moran@stms.co.il>
2019-09-18 18:33:08 +0300

    fix

---
+++ script-small.sh
@@ -0,0 +1,72 @@
+apk add curl
+
+# we dont want to create the index pattern over and over again, so I am cheching if it exists first
+
+code=$(curl -X GET "kibana:5601/api/saved_objects/_find?type=index-pattern&search_fields=title&search=logstash*" -H 'kbn-xsrf: true')
+if [[ $code =~ '"total":0' ]] ; then
+
+	curl -X POST -D- 'kibana:5601/api/saved_objects/index-pattern' \
+		-H 'Content-Type: application/json' \
+		-H 'kbn-version: 7.2.0' \
+		-d '{"attributes":{"title":"logstash*","timeFieldName":"@timestamp"}}'
+		
+	echo "Creating Index Template"
+		
+	curl -X PUT "elasticsearch:9200/_template/softov_log" -H 'Content-Type: application/json' -d @index_template.cfg
+
+fi
+
+# Send fake msg to create an index
+
+logger -n 127.0.0.1 -P 514 "Moran's test MSG"
+	
+# Create the Clean Policy
+
+curl -X PUT 'elasticsearch:9200/_ilm/policy/logstash_clean_policy?pretty' -H 'Content-Type: application/json' -d '
+{ 
+  "policy": {
+    "phases": {
+      "hot": {
+        "min_age": "0ms",
+        "actions": {
+          "set_priority": {
+            "priority": 100
+          }
+        }
+      },
+      "delete": {
+        "min_age": "14d",
+        "actions": {
+          "delete": {}
+        }
+      }
+    }
+  }
+}'
+
+# Assign the Clean Policy to the index
+
+curl -X PUT 'elasticsearch:9200/_template/logstash_clean_policy1?pretty' -H 'Content-Type: application/json' -d '
+{
+  "index_patterns": [
+    "logstash*"
+  ],
+  "settings": {
+    "number_of_shards": 1,
+    "number_of_replicas": 1,
+    "index.lifecycle.name": "logstash_clean_policy"
+  }
+}'
+
+# Update Fields properties for existing indexes
+
+curl -X PUT "elasticsearch:9200/logstash*/_mapping?pretty" -H 'Content-Type: application/json' -d '
+{
+  "properties": {
+        "message": { "type": "text"  },
+		    "duration": { "type": "long"  },
+		    "service_duration": { "type": "long"  },
+        "seq": { "type": "long"  }
+  }
+}'
+
