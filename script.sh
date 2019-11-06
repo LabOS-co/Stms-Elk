c7bc5e96fe40429cd96b1c0d0dfe9d1664948aed
netlims <moran@stms.co.il>
2019-11-06 15:12:50 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,12 @@
 apk add curl
 apk add util-linux
 
+
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:5601/status -I)" != "200" ]]; 
+	echo 'Kibana is not ready... sleeping for 5';
+	do sleep 5; 
+done
+
 # we dont want to create the index pattern over and over again, so I am cheching if it exists first
 
 code=$(curl -X GET "kibana:5601/api/saved_objects/_find?type=index-pattern&search_fields=title&search=logstash*" -H 'kbn-xsrf: true')
